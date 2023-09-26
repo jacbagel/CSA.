6d9175de0f6dffc65c0d38eeea212aba2ed89c1d
jacbagel <jacquelinebehaeghel@billericak12.com>
2023-09-25 23:51:31 -0400

    mathex3

--- mathex.java
+++ mathex.java
@@ -9,6 +9,15 @@ public class mathex {
 
         timeLeft(1,15,4,36);
         timeLeft(1,34,8,20);
+
+       
+        System.out.println(dayOfWeek(0,1));
+        System.out.println(dayOfWeek(0,14));
+        System.out.println(dayOfWeek(6,22));
+        System.out.println(dayOfWeek(5,4));
+        System.out.println(dayOfWeek(1,24));
+        System.out.println(dayOfWeek(2,1));
+
     }
     public static int swap(int x){
         int hundred = x/100*100;
@@ -25,11 +34,14 @@ public class mathex {
       int deptimem = (furtime- mytime)%60;
       System.out.println( deptimeh + " hours and " + deptimem + " minutes");
 
+    }
+    public static int dayOfWeek(int pos_jan_1, int date_in_jan){
+        // int number_of_weeks = date_in_jan%7;
+        // int position = date_in_jan - (7*number_of_weeks);
+        int position = (date_in_jan + 6 + pos_jan_1)%7 ;
+        return position;
 
-
-
-      
-
+        //day of month(date in jan) % 7 = full weeks untill day of week, take day - 7 * full weeks = answer
 
     }
 }
