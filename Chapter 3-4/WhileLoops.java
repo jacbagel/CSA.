bebe003735f616dc513ae1c758e9243f95e6fd76
jacbagel <jacquelinebehaeghel@billericak12.com>
2023-12-08 13:47:53 -0500

    done 11/08

--- Chapter 3-4/WhileLoops.java
+++ Chapter 3-4/WhileLoops.java
@@ -1,18 +1,24 @@
 public class WhileLoops {
     public static void main(String [] args){
         WhileLoops ab = new WhileLoops();
-        // System.out.println(ab.addOdds(5));
-        // System.out.println(ab.addOdds(37));
-        // System.out.println(ab.addOdds(100));
+        System.out.println(ab.addOdds(5));
+        System.out.println(ab.addOdds(37));
+        System.out.println(ab.addOdds(100));
 
-        // System.out.println(ab.sumDigits(5384)); //20
-        // System.out.println(ab.sumDigits(12)); //3
-        // System.out.println(ab.sumDigits(456)); //15
+        System.out.println(ab.sumDigits(5384)); //20
+        System.out.println(ab.sumDigits(12)); //3
+        System.out.println(ab.sumDigits(456)); //15
 
-        // System.out.println(ab.howManyYears(111.2, 120)); //1
-        // System.out.println(ab.howManyYears(111.2, 150)); //3
+        System.out.println(ab.howManyYears(111.2, 120)); //7
+        System.out.println(ab.howManyYears(111.2, 150)); // 27
         
         ab.printSum(6); //21
+        ab.printSum(8); //36
+
+        System.out.println(ab.isPerfectSquare(25)); //true
+        System.out.println(ab.isPerfectSquare(24)); //false
+        System.out.println(ab.isPerfectSquare(49)); //true
+        System.out.println(ab.isPerfectSquare(13)); //false
     }
     
     public int addOdds(int n){
@@ -36,24 +42,49 @@ public class WhileLoops {
     }
 
     public int howManyYears(double startpop, double endpop){
-        // will reutrn x year to get to y population
+        // will reutrn y year to get to x population
         // going at a rate of 1.13 per year
         int y = 0;
         while(startpop <= endpop){
             y += 1;
-            startpop = startpop * 1.13;
+            startpop = (startpop * 0.0113) + startpop;
         }
         return y;
     }
 
     public void printSum(int n){
         // ex: n = 6, would print out 1+2+3+4+5+6=21
-        int sum = 1;
-        while (n >= 0){
-            n -= 1; // correct
-            //sum = sum%10;
-            sum += 1;
+        int sum = 0;
+        int x = 0;
+        while (n > 0){
+            n -= 1; 
+            sum ++;
+            sum = sum + n;
+            x++;
+            if (n == 0){
+                System.out.print(x + " ");
+            }
+            else{
+                System.out.print(x + " + ");
+            }
         }
-        System.out.println();
+        System.out.println("= " + sum);
+    }
+
+    public boolean isPerfectSquare(int n){
+        // write perfect suare as a sum of x consecutive odd numbers
+        // ex: n=25 so 1+3+5+7+9
+        int sum = 0;
+        int x = 1;
+        while ( sum < n ){
+            sum = sum + x;
+             x = x + 2;
+        }
+        if(sum == n){
+            return true;
+        }
+        else{
+            return false;
+        }
     }
 }
