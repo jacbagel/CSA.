341f0c97ea674191bea0e7d547dcb3b5dd72156a
jacbagel <jacquelinebehaeghel@billericak12.com>
2023-12-08 13:59:17 -0500

    12/08 not done

--- Chapter 3-4/WhileLoops.java
+++ Chapter 3-4/WhileLoops.java
@@ -5,26 +5,26 @@ public class WhileLoops {
         System.out.println(ab.addOdds(37));
         System.out.println(ab.addOdds(100));
 
-        System.out.println(ab.sumDigits(5384)); //20
-        System.out.println(ab.sumDigits(12)); //3
-        System.out.println(ab.sumDigits(456)); //15
+    //     System.out.println(ab.sumDigits(5384)); //20
+    //     System.out.println(ab.sumDigits(12)); //3
+    //     System.out.println(ab.sumDigits(456)); //15
 
-        System.out.println(ab.howManyYears(111.2, 120)); //7
-        System.out.println(ab.howManyYears(111.2, 150)); // 27
+    //     System.out.println(ab.howManyYears(111.2, 120)); //7
+    //     System.out.println(ab.howManyYears(111.2, 150)); // 27
         
-        ab.printSum(6); //21
-        ab.printSum(8); //36
+    //     ab.printSum(6); //21
+    //     ab.printSum(8); //36
 
-        System.out.println(ab.isPerfectSquare(25)); //true
-        System.out.println(ab.isPerfectSquare(24)); //false
-        System.out.println(ab.isPerfectSquare(49)); //true
-        System.out.println(ab.isPerfectSquare(13)); //false
-    }
+    //     System.out.println(ab.isPerfectSquare(25)); //true
+    //     System.out.println(ab.isPerfectSquare(24)); //false
+    //     System.out.println(ab.isPerfectSquare(49)); //true
+    //     System.out.println(ab.isPerfectSquare(13)); //false
+     }
     
     public int addOdds(int n){
         int sum = 1;
         int i = 1;
-        while(i>=1 && i<n){
+        while(i<n){
             i = i+2;
             sum = sum + i;
         }
